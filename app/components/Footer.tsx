d0d0b36cf966fdc9b6bf6a8d9518ea7ac5509ddb
Khazra Ayub <khazra.ayub786@gmail.com>
2024-11-10 04:47:41 -0800

    first commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,48 +1,24 @@
-
-"use client";
 import React from 'react'
-import Link from "next/link";
-import { MdEmail } from "react-icons/md";
-import { FaLinkedin } from "react-icons/fa";
-import { FaGithub } from "react-icons/fa";
-import { FaSquareFacebook } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa6";
 
-export default function Footer() {
-  return (
-    <div>
-      <footer style={{ background: "#111827", color: "white", }}>
-      <div className="foot">
-        <p id="copyright">
-           Copyright &copy; 2024 Designed by Khazra Shaikh &#9829; All right Reserved
-        </p>
-        
-        <ul>
-            <li className="animate">
-              <Link target="_blank"  href={"https://www.linkedin.com/in/khazra-ayub-99b762260/"}>
-                <FaLinkedin />
-              </Link>
-            </li>
 
-            <li className="animate"> 
-              <Link target="_blank" href={ "https://github.com/Khazraayub" }>
-                <FaGithub />
-              </Link>
-            </li>
-
-            <li className="animate">
-              <Link target="_blank" href={ "#" }>
-                <FaSquareFacebook />
-              </Link>
-            </li>
-
-            <li className="animate">
-              <Link target="_blank" href={ "#" }  >
-                <MdEmail />
-              </Link>
-            </li>
-          </ul>
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer">
+                <div className='social-links'>
+                    <a href="#"><FaFacebookF /></a>
+                    <a href="#"><FaInstagram /></a>
+                    <a href="https://www.linkedin.com/in/khazra-ayub-99b762260/"><FaLinkedin /></a>
+                    <a href="https://github.com/Khazraayub"><FaGithub /></a>
+                </div>
+                <p className='footer-text'>Created by <span className='name'>Khazra Shaikh.</span> | All Rights Reserved</p>
+            </footer>
         </div>
-    </footer>
-    </div>
-  );
+    )
 }
+
+export default Footer
